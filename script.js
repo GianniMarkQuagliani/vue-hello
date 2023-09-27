a46a0e0ef427b8640e55acaa7ad1baa770b35406
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-09-27 15:59:26 +0200

    [add] more comments

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const { createApp } = Vue;
 
+// creo l'app
 createApp({
+    // messaggio
     data () {
+        // creo un oggetto
         return {
             // creo un messaggio di default
             message: 'Hello Vue!',
+            // url dell'immagine
             imageUrl: 'https://unsplash.it/300/300?image=15'
         }
     }
-}).mount('#app')
+}).mount('#app') // metto l'app
