d910764ac6b94a8c49c36a77b974458506016b5e
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-09-27 15:43:34 +0200

    [fix] Hello Vue

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const { createApp } = Vue;
 createApp({
     data () {
         return {
+            // creo un messaggio di default
             message: 'Hello Vue!',
+            // creo un array vuoto
+            myArray: []
+
         }
     }
-})
+}).mount('#app')
